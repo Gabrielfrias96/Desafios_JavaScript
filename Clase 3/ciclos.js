10b544d150067119e0b2aad29909d4589abb5353
Gabriel Frias <gabriel.frias1996@gmail.com>
2021-09-09 14:58:59 -0300

    cambio en ciclos

--- Clase 3/ciclos.js
+++ Clase 3/ciclos.js
@@ -1,9 +1,9 @@
 alert (`Te mostraremos los 20 primeros multiplos del numero que tu elijas.`)
-var multiplosDe = prompt(`Ingresa un numero del 1 al 10`)
+let multiplosDe = prompt(`Ingresa un numero del 1 al 10`)
 
 for (var i = 1; i <= 100 ; i++){
-   var multiplos= i * multiplosDe;
-   console.log(`EL Multiplo numero ` + i + ` es = ` + multiplos)
+   let multiplos= i * multiplosDe;
+   console.log(`EL Multiplo numero ${i} es = ${multiplos}`)
    if (i == 25){
        break;
    }
