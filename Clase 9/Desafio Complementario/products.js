1920c4bec01717977a7014130ee9f32bd5e3b6ec
Gabriel Frias <gabriel.frias1996@gmail.com>
2021-10-06 23:54:04 -0300

    clase9 complementario

---
+++ Clase 9/Desafio Complementario/products.js
@@ -0,0 +1,28 @@
+let productos =[
+    {
+        nombre: "remera",
+        precio: 260
+    },
+
+    {
+        nombre: "remera",
+        precio: 425
+    },
+
+    {
+        nombre: "remera",
+        precio: 693
+    },
+    {
+        nombre: "remera",
+        precio: 878
+    },
+    {
+        nombre: "remera",
+        precio: 768
+    },
+    {
+        nombre: "remera",
+        precio: 580
+    }
+]
