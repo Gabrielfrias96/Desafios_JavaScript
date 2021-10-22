ee1e9b7263deee66fc4551d33c096d5e30b07307
Gabriel Frias <gabriel.frias1996@gmail.com>
2021-10-22 10:24:40 -0300

    actualizacion

--- Clase 13/index.js
+++ Clase 13/index.js
@@ -1,3 +1,16 @@
-$(`#view`).click(()=>{
-    $(`#box-productos`).fadeOut(500);
+$(`#no-view`).click(()=>{
+   $(`#tienda`).fadeOut();
 })  
+
+$(`#view`).click(()=>{
+   $(`#tienda`).fadeIn();
+})  
+
+
+$(`#button`).click(()=>{
+   let producto =$(`#input`).val();
+   $(`#box-productos`).append(`
+   <div>${producto}</div>`)
+   }) ;
+
+
