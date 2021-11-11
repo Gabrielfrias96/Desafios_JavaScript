eddc6f14bfc44413f75960cb071e030b127d57d8
Gabriel Frias <gabriel.frias1996@gmail.com>
2021-11-11 12:49:58 -0300

    hola

--- Clase 1/prompt.js
+++
@@ -1,6 +0,0 @@
-let nameUser = prompt ("Cual Es tu Nombre");
-alert ("Perfecto ahora sabemos que te llamas " + nameUser + "!");
-let ageUser = prompt ("Genial... y " + nameUser + " que edad tenes?");
-const fecha = 2021;
-let nacimiento = fecha - ageUser;
-alert ("Wow naciste en el año " +  nacimiento + " ¿No te parece increible " + nameUser + "?");
